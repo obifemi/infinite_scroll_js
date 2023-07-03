d574bbb7e4900501d456a7633431b1a48550b155
ali mert inal <mert.inal@gmail.com>
2023-07-03 17:14:36 +0200

    add functionality

---
+++ script.js
@@ -0,0 +1,61 @@
+const apiKey = 'WOT0rJeHR7nCezsaqdf1PSyG1gZYO_6TGhZFs6D6yEs'
+const count = 10
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+let photosArray = []
+
+
+
+const displayPhotos = () =>{
+    photosArray.forEach((photo) =>{
+        //create <a> to link to unsplash
+        const item = document.createElement('a')
+        item.setAttribute('href', photo.links.html) 
+        item.setAttribute('target', '_blank')
+        //create <img> for photo
+        const img = document.createElement('img')
+        img.setAttribute('src', photo.urls.regular)
+        img.setAttribute('alt', photo.alt_description)
+        img.setAttribute('title', photo.alt_description)
+        //put <img> inside <a>, then put both inside imageContainer
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+        })
+        ;
+}
+
+//get photos from api
+const  getPhotos = async () =>{
+    try{
+        const response = await fetch(apiUrl)
+        photosArray = await response.json()
+        console.log(photosArray)
+        displayPhotos()
+        
+        
+    }catch(error){
+        //catch error here
+    }
+
+}
+
+//on load
+getPhotos()
+
+
+
+
+
+
+
+//check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () =>{
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+        getPhotos()
+    }
+}
+)
+
+
